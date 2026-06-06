8e0f0fc015261c4089d681722d81088cf96e16c3
vova <vavakharitonov2004@gmail.com>
2026-06-06 14:19:14 +0300

    Add new allow origin for backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,7 @@ app = FastAPI(title="AiTheana", version="1.0.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://iamdisagreee.github.io/AiTheana/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
